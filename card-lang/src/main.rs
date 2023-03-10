615de6cc0ab19eba90dc3f60f84641c6e3f7c4ff
formula1 <samtobia@gmail.com>
2023-03-10 15:35:10 -0800

    DX: whitespace cleanup

--- card-lang/src/main.rs
+++ card-lang/src/main.rs
@@ -1,22 +1,22 @@
 
 mod calculator {
-    pub mod lexer;
-    pub mod parser;
+  pub mod lexer;
+  pub mod parser;
 }
 mod types {
-    pub mod Lexer;
-    pub mod Parser;
-    pub mod Evaluator;
-    pub mod ReusedStructs;
+  pub mod Lexer;
+  pub mod Parser;
+  pub mod Evaluator;
+  pub mod ReusedStructs;
 }
 
 use calculator::lexer::CalculatorLexer;
 use calculator::parser::CalculatorParser;
 
 fn main() {
-    let tokens = CalculatorLexer.tokenizeString(
-        "hello world".to_string()
-    );
+  let tokens = CalculatorLexer.tokenizeString(
+    "hello world".to_string()
+  );
   let trees = CalculatorParser.parse(tokens);
   println!("Hello, world!");
 }
