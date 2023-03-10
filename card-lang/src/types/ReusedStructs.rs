08a5f8b66b8ad4e2b0fcf6e4a2f4b39424576349
Samuel Tobia <sam@system76-pc.localdomain>
2023-03-10 15:17:50 -0800

    UF-Feat: Ok looking parser

--- card-lang/src/types/ReusedStructs.rs
+++ card-lang/src/types/ReusedStructs.rs
@@ -6,52 +6,78 @@ pub struct Token {
 }
 
 pub struct Node {
-  pub node_type: String,
+  pub node_type: NodeType,
   pub values: Option<HashMap<String, String>>,
-  pub branches: Option<HashMap<String, Node>>
+  pub branches: Option<HashMap<String, Node>>,
+  pub args: Option<Vec<Node>>,
+}
+
+pub enum NodeType {
+  ValueNode,
+  OperatorNode,
+  IdentifierNode,
+  AssignNode,
+  CallNode,
+  FunctionNode
+}
+
+enum PossibleNode {
+  ValueNode,
+  OperatorNode,
+  IdentifierNode,
+  AssignNode,
+  CallNode,
+  FunctionNode
 }
 
 impl Node {
   fn ValueNode(values: HashMap<String, String>) -> Node {
     return Node {
-      node_type: "value".to_string(),
+      node_type: NodeType::ValueNode,
       values: Some(values),
-      branches: None
+      branches: None,
+      args: None,
     }
   }
   fn OperatorNode(branches: HashMap<String, Node>) -> Node {
     return Node {
-      node_type: "operator".to_string(),
+      node_type: NodeType::OperatorNode,
       values: None,
-      branches: Some(branches)
+      branches: Some(branches),
+      args: None,
     }
   }
   fn IdentifierNode(values: HashMap<String, String>) -> Node {
     return Node {
-      node_type: "identifier".to_string(),
-      values: None
-      branches:
+      node_type: NodeType::IdentifierNode,
+      values: None,
+      branches: None,
+      args: None,
+
     }
   }
   fn AssignNode(values: HashMap<String, String>) -> Node {
     return Node {
-      node_type: "assign".to_string(),
-      values: None
-      branches:
+      node_type: NodeType::AssignNode,
+      values: None,
+      branches: None,
+      args: None,
     }
   }
   fn CallNode(values: HashMap<String, String>) -> Node {
     return Node {
-      node_type: "call".to_string(),
-      values: None
-      branches:
+      node_type: NodeType::CallNode,
+      values: None,
+      branches:None,
+      args: None,
     }
   }
   fn FunctionNode(values: HashMap<String, String>) -> Node {
     return Node {
-      node_type: "operator".to_string(),
-      values: None
-      branches:
+      node_type: NodeType::FunctionNode,
+      values: None,
+      branches: None,
+      args: None,
     }
   }
 }
